697d1653a55906faed8ba14b4cba714df8bf739c
Lucas Evangelista dos Santos <190989128+ZRGRU@users.noreply.github.com>
2025-05-12 20:36:05 -0300

    Refatora estrutura condicional na classe Ex10_Eleicao para melhorar a legibilidade

--- ListaJava01/Ex10_Eleicao.java
+++ ListaJava01/Ex10_Eleicao.java
@@ -5,16 +5,16 @@ public class Ex10_Eleicao {
         Util.imprimir("Digite a sua idade para saber qual sua classificação nas eleições: ");
         int idade = Util.lerValorInteiro();
         Util.imprimir("Sua classificação é: ");
-    if (idade < 16) {
-        Util.imprimir("Não eleitor.");
-    } else {
-        if (idade >17 && idade < 66) {
-            Util.imprimir("Eleitor obrigatório.");
-        } else {
-            Util.imprimir("Eleitor facultativo.");
+        
+        if (idade < 16) {
+            Util.imprimir("Não eleitor.");
+        } 
+        else {
+            if (idade >17 && idade < 66) {
+                Util.imprimir("Eleitor obrigatório.");
+            } else {
+                Util.imprimir("Eleitor facultativo.");
+            }
         }
-    }
-
-
     }
 }
