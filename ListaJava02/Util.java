113d005df7a80314ad0be6fddf47f70ac257ab3f
Lucas Evangelista <190989128+ZRGRU@users.noreply.github.com>
2025-05-19 22:12:31 -0300

    Adiciona classe Util com métodos utilitários para entrada e formatação de dados

---
+++ ListaJava02/Util.java
@@ -0,0 +1,46 @@
+
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class Util {
+
+    final static Scanner TECLADO_SCANNER = new Scanner(System.in);
+
+    public static void imprimir(String texto) {
+        System.out.println(texto);
+    }
+
+    public static int lerValorInteiro() {
+        int valor = 0;
+        valor = TECLADO_SCANNER.nextInt();
+        return valor;
+    }
+
+    public static double lerValorReal() {
+        double valor = 0;
+        valor = TECLADO_SCANNER.nextDouble();
+        return valor;
+    }
+
+    public static String lerTexto() {
+        String texto = null;
+        texto = TECLADO_SCANNER.nextLine();
+        return texto;
+    }
+
+    public static String lerPalavra() {
+        String palavra = null;
+        palavra = TECLADO_SCANNER.next();
+        return palavra;
+    }
+
+    public static char lerCharacter() {
+        char caracter = ' ';
+        caracter = TECLADO_SCANNER.next().charAt(0);
+        return caracter;
+    }
+
+    public static DecimalFormat df = new DecimalFormat("#0.00");
+
+}
